9467d5ae4928b425a1821e1346072c9361e12509
HaithamSalem9 <haitham.salem666@gmail.com>
2026-03-15 20:41:41 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,91 @@
+import express from "express";
+import bodyParser from "body-parser" ;
+
+
+const app= express();
+app.use(express.static("public"));
+const port = 3000;
+
+app.use(bodyParser.urlencoded( { extended : true}));
+
+app.set('view engine', 'ejs');
+
+app.use('/public', express.static('public', {
+    setHeaders: (res, path) => {
+        if (path.endsWith('.css')) {
+            res.setHeader('Content-Type', 'text/css');
+        }
+    }
+}));
+
+
+app.use('/public', express.static('public', {
+    setHeaders: (res, path) => {
+        if (path.endsWith('.css')) {
+            res.setHeader('Content-Type', 'text/css');
+        }
+    }
+}));
+
+
+app.get("/" , (req , res ) => {
+    res.render("index.ejs")
+});
+
+
+app.get ("/index2" , (req , res) => {
+    res.render("index2.ejs");
+})
+
+
+
+app.get ("/index3" , (req , res) => {
+    res.render("index3.ejs");
+})
+
+
+app.get ("/index4" , (req , res) => {
+    res.render("index4.ejs");
+})
+
+
+app.get ("/about_us" , (req , res) => {
+    res.render("about_us.ejs");
+})
+
+
+app.get ("/index5" , (req , res) => {
+    res.render("index5.ejs");
+})
+
+
+app.get ("/index6" , (req , res) => {
+    res.render("index6.ejs");
+})
+
+
+app.get ("/index7" , (req , res) => {
+    res.render("index7.ejs");
+})
+
+
+app.get ("/index8" , (req , res) => {
+    res.render("index8.ejs");
+})
+
+
+
+
+
+app.post("/submit", (req , res ) => {
+console.log("User Name : "+ req.body.user_name +" " +"Email : "+req.body.email );
+console.log("User Message : " +req.body.message );
+res.render("about_us.ejs");
+
+});
+
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
